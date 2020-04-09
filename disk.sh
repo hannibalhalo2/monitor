54bf1ec8dbda402e813bf4f6f2bebfd9e5f63807
hannibal <hannibal@console>
2020-04-09 19:35:41 -0400

    disk

--- disk.sh
+++ disk.sh
@@ -1,3 +1,6 @@
 a="$(df /dev/sda5 | tail -n +2 | head -n 1 | awk '{print $5}')"
 b="$(cut -d'%' -f1 <<<$a)"
 echo $b
+#crar fichero de 10gb
+#dd if=/dev/urandom of=random.img count=1024 bs=10M
+
--- load_data.sh
+++ load_data.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 . config.cfg
+#variable local
 #KEYWRITE=TFAC6T2E9MHSIIH6
 INSTALL_DIR=$(pwd)
 MEMORIA=$(${INSTALL_DIR}/memoria.sh)
 LOADAVG1=$(${INSTALL_DIR}/loadavg.sh)
 LOADAVG5=$(${INSTALL_DIR}/loadavg.sh 5)
 LOADAVG15=$(${INSTALL_DIR}/loadavg.sh 15)
+ROOT=$(${INSTALL_DIR}/disk.sh)
 #CON VARIABLE LOCAL
 #curl https://api.thingspeak.com/update?api_key=${KEYWRITE}\&field1=${MEMORIA}\&field2=${LOADAVG1}\&field3=${LOADAVG5}\&field4=${LOADAVG15}
 #CON VARIABLE DE ENTORNO
-curl https://api.thingspeak.com/update?api_key=${WRITEKEY}\&field1=${MEMORIA}\&field2=${LOADAVG1}\&field3=${LOADAVG5}\&field4=${LOADAVG15}
+curl https://api.thingspeak.com/update?api_key=${WRITEKEY}\&field1=${MEMORIA}\&field2=${LOADAVG1}\&field3=${LOADAVG5}\&field4=${LOADAVG15}\&field5=${ROOT}
